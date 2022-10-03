5075805864c63c26a46e947ff1139a7a240208cb
Viktoria <zaharova181114@gmail.com>
2022-10-03 18:51:47 +0300

    add pages

--- app/js/common.js
+++ app/js/common.js
@@ -10,3 +10,15 @@ $('.btn-burger').on('click', function () {
    $(this).toggleClass('open');
    $('.header-wrapper').fadeToggle();
 });
+
+$('.go_to').click(function (e) {
+    e.preventDefault();
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length !== 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    return false;
+});
+
